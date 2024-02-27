8f32078b8bd583317c6414c3943404284889dc2c
Bogdan Safronov <bogdasafronov@gmail.com>
2024-02-27 17:20:36 +0300

    feat(user): add user find-by methods

---
+++ src/user/user.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class UserService {
+  constructor(private prismaService: PrismaService) {}
+
+  async findById(id: number) {
+    return await this.prismaService.user.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async findByUsername(username: string) {
+    return await this.prismaService.user.findUnique({
+      where: {
+        username,
+      },
+    });
+  }
+
+  async findByEmail(email: string) {
+    return this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+    });
+  }
+}
