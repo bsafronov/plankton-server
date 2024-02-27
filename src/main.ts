7606768c8d9d38539840d2132545f94111a1f5ee
Bogdan Safronov <bogdasafronov@gmail.com>
2024-02-27 18:13:40 +0300

    feat(app): add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      whitelist: true,
+    }),
+  );
   app.enableCors();
   await app.listen(3000, () =>
     console.log('Server running on http://localhost:3000'),
