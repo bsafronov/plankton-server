23dcd3c82bfedffaf21a9482b5420d326ef1a11c
Bogdan Safronov <bogdasafronov@gmail.com>
2024-02-27 17:22:00 +0300

    feat(app): add modules, enable cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,17 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UserModule,
+    JwtModule.register({
+      global: true,
+      secret: 'TEST_SECRET',
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
 })
 export class AppModule {}
